44c0b87d554fedec7645e04ad21b4c446aaa82e1
Nicolas Ali <alinicolas006@gmail.com>
2026-03-27 17:28:42 +0200

    Finished first iteration of grep functionality.

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::{env, fs::File, io::BufRead, io::BufReader, path::Path};
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let path = if args[1].len() > 1 {
+        Path::new(&args[1])
+    } else {
+        panic!("No directory selected");
+    };
+
+    let searched_word = if args[2].len() > 1 {
+        &args[2]
+    } else {
+        panic!("No word provided to search for");
+    };
+
+    let file = File::open(path)?;
+
+    let file_read = BufReader::new(file);
+
+    let mut current_line_index = 0;
+
+    for line in file_read.lines() {
+        current_line_index = current_line_index + 1;
+
+        let line = line?;
+
+        for word in line.split(' ') {
+            //Debugging line:
+
+            //println!("Comparing [{:?}] to [{:?}]", word, searched_word);
+
+            if &String::from(word) == searched_word {
+                println!(
+                    "instance of {} on line {}",
+                    &searched_word, &current_line_index
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
